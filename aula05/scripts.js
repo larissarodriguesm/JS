07ce17d8f0da9f10f876b9cd90195ab34df122bd
bruno-GSilva <brunogomesdsilva8@gmail.com>
2023-01-23 21:36:21 -0300

    aula05

---
+++ aula05/scripts.js
@@ -0,0 +1,47 @@
+// var resposta = confirm("Boa noite")
+
+// console.log(resposta)
+
+// if(resposta) {
+//     alert("Obrigado por aceitar")
+// } else {
+//     confirm("Boa noite")
+// }
+
+// Para inserir uma repetição sempre que clicar em cancelar:
+// while(!confirm("Boa noite")){
+
+// }
+
+// var resposta = prompt("Qual sua idade?")
+
+// if(resposta>=18) {
+//     alert("Você é MAIOR de idade.")
+// }else if(resposta == null){
+//     alert("Inserir uma resposta.")
+// }else {
+//     alert("Você é MENOR de idade.")
+// }
+    //alert(resposta)
+
+// let tvEstaLigada = true;
+
+// function power() {
+//     if(tvEstaLigada){
+//         desligar()
+//     }else {
+//         ligar()
+//     }
+// }
+
+// function desligar() {
+//     alert("Desligando tv")
+//     tvEstaLigada = false
+// }
+
+function isTruthy (valor) {
+    return !!valor
+}
+
+console.log(isTruthy("0"));
+
